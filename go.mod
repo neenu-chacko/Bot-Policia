066403e9d8ab3a2f5e5544e986d7ddef04332db8
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2020-04-02 21:15:17 +0530

    Added basic talk skills
    Still a nooooob Bot

--- go.mod
+++ go.mod
@@ -5,5 +5,4 @@ go 1.14
 require (
 	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
-	github.com/yanzay/tbot v1.0.0
 )
--- main.go
+++ main.go
@@ -1,25 +1,41 @@
-
 package main
+
 import (
-		tgbot "github.com/go-telegram-bot-api/telegram-bot-api"
-		"fmt"
+	"fmt"
+
+	tgbot "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
 func main() {
-		bot, err := tgbot.NewBotAPI("906142594:AAGp2PgkOUWFNmcFq0fazTU8APVmGSgpaPk")
-		if err != nil {
-			panic(err)
-			return
-		}
-	  fmt.Print("Bot Connected Successfully!\n")
-	  u := tgbot.NewUpdate(0)
-	  updates,err := bot.GetUpdatesChan(u)
+	bot, err := tgbot.NewBotAPI("906142594:AAGp2PgkOUWFNmcFq0fazTU8APVmGSgpaPk")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print("Bot Connected Successfully!\n")
 
-	  for update := range updates{
-		  if update.Message == nil{
-			  continue
-		  }
-		  msg := tgbot.NewMessage(update.Message.Chat.ID,"Aaha itharith!! Keri Vaa")
-		  bot.Send(msg)
-	  }
+	responses := map[string]func() string{
+		"/start":    func() string { return "Nice to meet you!" },
+		"hi":        func() string { return "Hi!" },
+		"poli":      func() string { return "sanam" },
+		"Go Corona": func() string { return "Corona Go" },
+		"stayhome":  func() string { return "#veettilirimyre" },
+	}
+
+	u := tgbot.NewUpdate(0)
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		resp, ok := responses[update.Message.Text]
+		if !ok {
+			msg := tgbot.NewMessage(update.Message.Chat.ID, "Jaba jaba, Jabaabi jaba!")
+			bot.Send(msg)
+			continue
+		}
+		msg := tgbot.NewMessage(update.Message.Chat.ID, resp())
+		bot.Send(msg)
+	}
 }
